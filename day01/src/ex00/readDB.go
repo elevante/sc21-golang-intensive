484e14e3125aa1e7e5eb1018d1cf9d4d91f88a66
Adam Gahverdiev <adam_gahverdiev@mail.ru>
2023-07-12 10:04:27 +0300

    sc_day01/wip: added struct for parsing files .json and .xml

--- day01/src/ex00/readDB.go
+++ day01/src/ex00/readDB.go
@@ -1,1 +1,20 @@
-package ex00
+package main
+
+type Ingredients struct {
+	Ingredient_name  string `json:"ingredient_name" xml:"itemname"`
+	Ingredient_count string `json:"ingredient_count" xml:"itemcount"`
+	Ingredient_unit  string `json:"ingredient_unit" xml:"itemunit,omitempty"`
+}
+type Cakes struct {
+	Name       string `json:"name" xml:"name"`
+	Time       string `json:"time" xml:"stovetime"`
+	Ingredient []Ingredients
+}
+
+type Recipes struct {
+	Cake []Cakes `json:"cake" xml:"cake"`
+}
+
+func main() {
+
+}
