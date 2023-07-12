45162d1dbfc15d3878724e503c75505c15a4b634
Adam Gahverdiev <adam_gahverdiev@mail.ru>
2023-07-12 10:47:47 +0300

    sc_day01/fix: refactoring struct

--- day01/src/ex00/readDB.go
+++ day01/src/ex00/readDB.go
@@ -1,18 +1,18 @@
 package main
 
 type Ingredients struct {
-	Ingredient_name  string `json:"ingredient_name" xml:"itemname"`
-	Ingredient_count string `json:"ingredient_count" xml:"itemcount"`
-	Ingredient_unit  string `json:"ingredient_unit,omitempty" xml:"itemunit"`
+	IngredientName  string `json:"ingredient_name" xml:"itemname"`
+	IngredientCount string `json:"ingredient_count" xml:"itemcount"`
+	IngredientUnit  string `json:"ingredient_unit,omitempty" xml:"itemunit"`
 }
-type Cakes struct {
-	Name       string        `json:"name" xml:"name"`
-	Time       string        `json:"time" xml:"stovetime"`
-	Ingredient []Ingredients `json:"ingredients" xml:"ingredients>item"`
+type Cake struct {
+	Name        string        `json:"name" xml:"name"`
+	Time        string        `json:"time" xml:"stovetime"`
+	Ingredients []Ingredients `json:"ingredients" xml:"ingredients>item"`
 }
 
 type Recipes struct {
-	Cake []Cakes `json:"cake" xml:"cake"`
+	Cake []Cake `json:"cake" xml:"cake"`
 }
 
 var jsonReader Recipes
