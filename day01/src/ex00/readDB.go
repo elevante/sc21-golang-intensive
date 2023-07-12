ad24ade7fa8c0f695ec3ef1b091d4849c6a3bfb9
Adam Gahverdiev <adam_gahverdiev@mail.ru>
2023-07-12 12:54:09 +0300

    sc_day01/wip: Implemented the functionality of reading json or xml and converting json to xml and vice versa. Removed comment in json file

--- day01/src/ex00/readDB.go
+++ day01/src/ex00/readDB.go
@@ -1,5 +1,20 @@
 package main
 
+import (
+	"encoding/json"
+	"encoding/xml"
+	"flag"
+	"fmt"
+	"os"
+	"path/filepath"
+	"strings"
+)
+
+type DBReader interface {
+	Read(file string) Recipes
+	Convert() []byte
+}
+
 type Ingredients struct {
 	IngredientName  string `json:"ingredient_name" xml:"itemname"`
 	IngredientCount string `json:"ingredient_count" xml:"itemcount"`
@@ -15,9 +30,70 @@ type Recipes struct {
 	Cake []Cake `json:"cake" xml:"cake"`
 }
 
-var jsonReader Recipes
-var xmlReader Recipes
+type JSONReader Recipes
+type XMLReader Recipes
+
+func (j *JSONReader) Read(file string) Recipes {
+	jsonFile, _ := os.ReadFile(file)
+	err := json.Unmarshal([]byte(jsonFile), &j)
+	if err != nil {
+		panic(err)
+	}
+	return Recipes(*j)
+}
+
+func (j *JSONReader) Convert() []byte {
+
+	data, err := xml.MarshalIndent(j, "", "    ")
+
+	if err != nil {
+		panic(err)
+	}
+
+	return data
+}
+
+func (x *XMLReader) Read(file string) Recipes {
+	jsonFile, _ := os.ReadFile(file)
+	err := xml.Unmarshal([]byte(jsonFile), &x)
+
+	if err != nil {
+		panic(err)
+	}
+	return Recipes(*x)
+}
+
+func (x *XMLReader) Convert() []byte {
+
+	data, err := json.MarshalIndent(x, "", "    ")
+
+	if err != nil {
+		panic(err)
+	}
+
+	return data
+}
 
 func main() {
+	Start()
+}
 
+func Start() {
+
+	FlagF := flag.String("f", "", "")
+	flag.Parse()
+
+	if strings.HasSuffix(*FlagF, ".xml") {
+		var x XMLReader
+		Parse(*FlagF, &x)
+	}
+	if strings.HasSuffix(*FlagF, ".json") {
+		var j JSONReader
+		Parse(*FlagF, &j)
+	}
+}
+
+func Parse(file string, reader DBReader) {
+	path := filepath.Join("../", "files/", file)
+	fmt.Printf("%v\n\n%v", reader.Read(path), string(reader.Convert()))
 }
