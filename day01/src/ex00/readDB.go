77c8d9f32313e57d584f59feacbec25e9ac98a53
Adam Gahverdiev <adam_gahverdiev@mail.ru>
2023-07-12 10:46:09 +0300

    sc_day01/fix: fixed struct for parsing .json and .xml

--- day01/src/ex00/readDB.go
+++ day01/src/ex00/readDB.go
@@ -3,17 +3,20 @@ package main
 type Ingredients struct {
 	Ingredient_name  string `json:"ingredient_name" xml:"itemname"`
 	Ingredient_count string `json:"ingredient_count" xml:"itemcount"`
-	Ingredient_unit  string `json:"ingredient_unit" xml:"itemunit,omitempty"`
+	Ingredient_unit  string `json:"ingredient_unit,omitempty" xml:"itemunit"`
 }
 type Cakes struct {
-	Name       string `json:"name" xml:"name"`
-	Time       string `json:"time" xml:"stovetime"`
-	Ingredient []Ingredients
+	Name       string        `json:"name" xml:"name"`
+	Time       string        `json:"time" xml:"stovetime"`
+	Ingredient []Ingredients `json:"ingredients" xml:"ingredients>item"`
 }
 
 type Recipes struct {
 	Cake []Cakes `json:"cake" xml:"cake"`
 }
+
+var jsonReader Recipes
+var xmlReader Recipes
 
 func main() {
 
