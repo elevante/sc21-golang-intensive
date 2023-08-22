42bf7baebd9d8673c8e49ac2a2b3883ae3449366
Adam Gahverdiev <adam_gahverdiev@mail.ru>
2023-08-22 23:49:47 +0300

    sc_day02/wip: added basic logic for ex03

--- day02/src/ex03/main.go
+++ day02/src/ex03/main.go
@@ -1,7 +1,45 @@
 package main
 
-import "fmt"
+import (
+	"flag"
+	"fmt"
+	"os"
+	"strconv"
+	"strings"
+	"time"
+)
 
 func main() {
-	fmt.Println("ex03")
+
+	var files []string
+	var sl []string
+
+	FlagA := flag.Bool("a", false, "")
+
+	flag.Parse()
+
+	files = flag.Args()
+
+	if !*FlagA {
+		fmt.Println(files)
+
+		for _, file := range files {
+			first := strings.Split(file, ".")
+			fmt.Println(first[0])
+			sec := time.Now().Unix()
+			newFile := first[0] + "_" + strconv.FormatInt(sec, 10) + "." + "tag.gz"
+			sl = append(sl, newFile)
+			fmt.Println(sl)
+		}
+
+		for _, file := range sl {
+			fileO, err := os.Create(file)
+			if err != nil { // если возникла ошибка
+				fmt.Println("Unable to create file:", err)
+				os.Exit(1) // выходим из программы
+			}
+			defer fileO.Close() // закрываем файл
+		}
+	}
+
 }
